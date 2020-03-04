5526c056542d3797839cfc86f19331c2944a1b61
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-03-04 12:14:52 -0800

    authenticate with env variables

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
   
-  http_basic_authenticate_with name: "Jungle", password: "Book"
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all
