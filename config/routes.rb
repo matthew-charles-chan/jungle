d927ce3074191997e86e879e99c499cf10f3660c
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-03-04 21:53:08 +0000

    feature/about-page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  get '/about' => 'about#show'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -16,5 +18,6 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
 
 end
